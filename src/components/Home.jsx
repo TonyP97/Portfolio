e397efb1e444d425ef1bd9301f4005fa61f9f223
Agustín <agustinpincheira97@gmail.com>
2022-11-04 12:01:59 -0300

    updates cv

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -166,6 +166,9 @@ return (
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/nodejs/nodejs-original.svg" title="Node.js" alt="Node.js" width="60px" height="60px" className="animate-[spin_1s]"/>
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/express/express-original.svg" title="Express" alt="Express" width="60px" height="60px" className="animate-[spin_1s] bg-gray-400"/>
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/postgresql/postgresql-original.svg" title="PostgreSQL" alt="PostgreSQL" width="60px" height="60px" className="animate-[spin_1s]"/>
+                            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/mysql/mysql-original.svg" title="MySQL" alt="MySQL" width="60px" height="60px" className="animate-[spin_1s] bg-gray-400"/>
+                            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/php/php-original.svg" title="PHP" alt="PHP" width="60px" height="60px" className="animate-[spin_1s]"/>
+                            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/apache/apache-original.svg" title="Apache" alt="Apache" width="60px" height="60px" className="animate-[spin_1s]"/>
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sequelize/sequelize-original.svg" title="Sequelize" alt="Sequelize" width="60px" height="60px" className="animate-[spin_1s]"/>
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-plain-wordmark.svg" title="HTML" alt="HTML" width="60px" height="60px" className="animate-[spin_1s]"/>
                             <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-plain-wordmark.svg" title="CSS" alt="CSS" width="60px" height="60px" className="animate-[spin_1s]"/>
@@ -212,8 +215,8 @@ return (
                 </div>
                 <div className="flex items-center">
                     <div className="sm:hidden md:hidden lg:hidden xl:hidden">
-                        <button title="Español" onClick={() => setLanguage(!language)}className="bg-[#08081f] hover:bg-yellow-400 border rounded-tl-md rounded-bl-md border-r-0"><span className="text-yellow-200 hover:text-[#08081f] px-0.5">ES</span></button>
-                        <button disabled title="English"  className="bg-yellow-200 border-yellow-200 border-yellow-200 border rounded-tr-md rounded-br-md border-l-0"><span className="text-[#08081f] px-0.5">EN</span></button>
+                        <button title="Español" onClick={() => setLanguage(!language)}className="bg-[#08081f] hover:bg-yellow-400 border-yellow-200 border rounded-tl-md rounded-bl-md border-r-0"><span className="text-yellow-200 hover:text-[#08081f] px-0.5">ES</span></button>
+                        <button disabled title="English"  className="bg-yellow-200 border-yellow-200 border rounded-tr-md rounded-br-md border-l-0"><span className="text-[#08081f] px-0.5">EN</span></button>
                     </div>
                     <button type="button" onClick={() => setNavbarOpen(!navbarOpen)} class="inline-flex items-center p-2 mx-2 text-sm text-yellow-200 rounded-lg md:hidden hover:bg-[#08081f] focus:outline-none focus:ring-2 focus:ring-yellow-400">
                     <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -246,8 +249,8 @@ return (
                     </li>
                     <li>
                         <div className="hidden sm:block md:block lg:block xl:block md:animate-[spin_1s]">
-                            <button title="Español" onClick={() => setLanguage(!language)}className="bg-[#08081f] hover:bg-yellow-400 border rounded-tl-md rounded-bl-md border-r-0"><span className="text-yellow-200 hover:text-[#08081f] px-0.5">ES</span></button>
-                            <button disabled title="English"  className="bg-yellow-200 border-yellow-200 border-yellow-200 border rounded-tr-md rounded-br-md border-l-0"><span className="text-[#08081f] px-0.5">EN</span></button>
+                            <button title="Español" onClick={() => setLanguage(!language)}className="bg-[#08081f] hover:bg-yellow-400 border-yellow-200 border rounded-tl-md rounded-bl-md border-r-0"><span className="text-yellow-200 hover:text-[#08081f] px-0.5">ES</span></button>
+                            <button disabled title="English"  className="bg-yellow-200 border-yellow-200 border rounded-tr-md rounded-br-md border-l-0"><span className="text-[#08081f] px-0.5">EN</span></button>
                         </div>
                     </li>
                     </ul>
@@ -305,6 +308,9 @@ return (
                             <img className="animate-[spin_1s]" src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/nodejs/nodejs-original.svg" title="Node.js" alt="Node.js" width="60px" height="60px"/>
                             <img className="bg-gray-400 animate-[spin_1s]" src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/express/express-original.svg" title="Express" alt="Express" width="60px" height="60px"/>
                             <img className="animate-[spin_1s]" src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/postgresql/postgresql-original.svg" title="PostgreSQL" alt="PostgreSQL" width="60px" height="60px"/>
+                            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/mysql/mysql-original.svg" title="MySQL" alt="MySQL" width="60px" height="60px" className="animate-[spin_1s] bg-gray-400"/>
+                            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/php/php-original.svg" title="PHP" alt="PHP" width="60px" height="60px" className="animate-[spin_1s]"/>
+                            <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/apache/apache-original.svg" title="Apache" alt="Apache" width="60px" height="60px" className="animate-[spin_1s]"/>
                             <img className="animate-[spin_1s]" src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sequelize/sequelize-original.svg" title="Sequelize" alt="Sequelize" width="60px" height="60px"/>
                             <img className="animate-[spin_1s]" src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-plain-wordmark.svg" title="HTML" alt="HTML" width="60px" height="60px"/>
                             <img className="animate-[spin_1s]" src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-plain-wordmark.svg" title="CSS" alt="CSS" width="60px" height="60px"/>
